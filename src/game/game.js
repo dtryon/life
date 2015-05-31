bb4c1a2486f256f4eb3ac19bf83cc4f972d5f2c2
Davin Tryon <davintryon@gmail.com>
2015-05-31 22:36:57 +0100

    basic game functionality

---
+++ src/game/game.js
@@ -0,0 +1,96 @@
+'use strict';
+
+function Game(gridSize) {
+	this.gridSize = gridSize;
+
+	this._grid = [];
+	for (var x = gridSize - 1; x >= 0; x--) {
+		var row = [];
+		for (var y = gridSize - 1; y >= 0; y--) {
+			row.push(0);
+		}
+		this._grid.push(row);
+	}
+}
+
+Game.prototype.grid = function() {
+	return this._grid;
+};
+
+Game.prototype.setCell = function(x, y) {
+	this._grid[x][y] = 1;
+};
+
+Game.prototype.unSetCell = function(x, y) {
+	this._grid[x][y] = 0;
+};
+
+Game.prototype.setCells = function(arr) {
+	for (var i = arr.length - 1; i >= 0; i--) {
+		this._grid[arr[i].x][arr[i].y] = 1;
+	};
+};
+
+Game.prototype.nextFrame = function () {
+
+	function countRow(row, prevColumn, nextColumn, isNotCurrent) {
+		var result = 0;
+		if (row) {
+			if (prevColumn) {
+				result += row[prevColumn];
+			}
+			if (nextColumn) {
+				result += row[nextColumn];
+			}
+			if (isNotCurrent) {
+				result += row[prevColumn+1]
+			}
+		}
+		return result;
+	}
+
+	var gridClone = this._grid.map(function(arr) {
+	    return arr.slice();
+	});
+
+	for (var x = gridClone.length - 1; x >= 0; x--) {
+		var currentRow = gridClone[x],
+		prevRow = null,
+		nextRow = null;
+
+		if (x > 0) {
+			prevRow = gridClone[x-1];
+		}
+		if (x < (gridClone.length - 1)) {
+			nextRow = gridClone[x+1];
+		}
+
+		for (var y = currentRow.length - 1; y >= 0; y--) {
+			var count = 0,
+			prevColumn = null,
+			nextColumn = null;
+
+			if (y > 0) {
+				prevColumn = y-1;
+			}
+
+			if (y < (currentRow.length - 1)) {
+				nextColumn = y+1;
+			}
+
+			count += countRow(prevRow, prevColumn, nextColumn, true);
+			count += countRow(nextRow, prevColumn, nextColumn, true);
+			count += countRow(currentRow, prevColumn, nextColumn);
+
+			if (count < 2 || count > 3) {
+				this._grid[x][y] = 0;
+			}
+
+			if (count === 3) {
+				this._grid[x][y] = 1;
+			}
+		};
+	};
+
+};
+
---
+++ tests/game/game-test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+/*
+Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+Any live cell with two or three live neighbours lives on to the next generation.
+Any live cell with more than three live neighbours dies, as if by overcrowding.
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*/
+
+describe('Game of Life', function () {
+
+	var game;
+
+	beforeEach(function () {
+		game = new Game(10);
+	});
+
+	it('should create a new 10 x 10 grid', function () {
+		expect(game.grid().length).toBe(10);
+		expect(game.grid()[0].length).toBe(10);
+	});
+
+	it('should set cell', function () {
+		game.setCell(1,1);
+		expect(game.grid()[1][1]).toBe(1);
+	});
+
+	it('should unset cell', function () {
+		game.setCell(1,1);
+		game.unSetCell(1,1);
+		expect(game.grid()[1][1]).toBe(0);
+	});
+
+	it('should set cells', function () {
+		game.setCells([{x:1,y:1},{x:2,y:2}]);
+		expect(game.grid()[1][1]).toBe(1);
+		expect(game.grid()[2][2]).toBe(1);
+	});
+
+	describe('Any live cell with fewer than two live neighbours dies, as if caused by under-population.', function () {
+		it('should kill cell with no live neighbours', function () {
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+
+		it('should kill cell with one live neighbour', function () {
+			game.setCell(3,2);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+
+		it('should kill cell with one live neighbour in the previous row', function () {
+			game.setCell(2,2);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+
+		it('should kill cell with one live neighbour in the next row', function () {
+			game.setCell(4,4);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+	});
+	
+	describe('Any live cell with two or three live neighbours lives on to the next generation.', function () {
+
+		it('should save cell with two live neighbours', function () {
+			game.setCell(3,2);
+			game.setCell(3,4);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(1);
+		});
+
+		it('should save cell with three live neighbours', function () {
+			game.setCell(3,2);
+			game.setCell(3,4);
+			game.setCell(4,4);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(1);
+		});
+
+		it('should save cell with three live neighbours on different rows', function () {
+			game.setCell(2,2);
+			game.setCell(3,4);
+			game.setCell(4,4);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(1);
+		});
+	});
+
+	describe('Any live cell with more than three live neighbours dies, as if by overcrowding.', function () {
+
+		it('should kill cell with four live neighbours on different rows', function () {
+			game.setCell(2,2);
+			game.setCell(3,4);
+			game.setCell(4,4);
+			game.setCell(4,3);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+
+		it('should kill cell with five live neighbours on different rows', function () {
+			game.setCell(2,2);
+			game.setCell(3,4);
+			game.setCell(4,4);
+			game.setCell(4,3);
+			game.setCell(4,2);
+
+			game.setCell(3,3);
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(0);
+		});
+	});
+
+	describe('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', function () {
+
+		it('should set cell with three live neighbours on different rows', function () {
+			game.setCell(2,2);
+			game.setCell(3,4);
+			game.setCell(4,4);
+
+			game.nextFrame();
+			expect(game.grid()[3][3]).toBe(1);
+		});
+	});
+});
